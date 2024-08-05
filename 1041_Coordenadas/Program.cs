d7c24a0fac920cd0525a44a666999ceae9a7b51d
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2024-08-05 10:13:04 -0300

    primeiro commit

---
+++ 1041_Coordenadas/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1041_Coordenadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y;
+
+            String[] vet = Console.ReadLine().Split(' ');
+            x = float.Parse(vet[0]);
+            y = float.Parse(vet[1]);
+
+            if (x == 0.0 && y == 0.0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x == 0.0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if (y == 0.0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (x > 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0.0 & y < 0.0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else
+            {
+                Console.WriteLine("Q4");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
